c1e86143077a3e50b0fbf1e1f6595fcd719d0ddc
Prateek Yadav <yprateek721@gmail.com>
2026-04-18 16:12:50 +0530

    Refactor linearSearch function return statements

--- Arrays/Linear_Search.cpp
+++ Arrays/Linear_Search.cpp
@@ -20,10 +20,10 @@ using namespace std;
 int linearSearch(vector<int>& arr, int target) {
     for(int i = 0; i < arr.size(); i++) {
         if(arr[i] == target) {
-            return i; // index found
+            return i; 
         }
     }
-    return -1; // not found
+    return -1; 
 }
 
 int main() {
