b9cdb493f22f9f1db32e625db8d20acc8965f2f5
Prateek Yadav <yprateek721@gmail.com>
2026-06-05 13:57:19 +0530

    Implement basic linked list node and main method

---
+++ Linked_List/Begin.java
@@ -0,0 +1,18 @@
+class Node{
+  public int data;
+  public Node next;
+
+  Node(int data1, Node next1)
+  {
+    this.data = data1;
+    this.next = next1;
+  }
+}
+
+public class Begin{
+  public static void main(String a[]){
+  int[] arr = {1,3,4,5);
+  Node y = new Node(arr[0],null);
+    System.out.println(y.data);
+  }
+}
