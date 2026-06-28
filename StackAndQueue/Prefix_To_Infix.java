26cf2cc11aac8355ec4c382e7796f3d918cdc695
Prateek Yadav <yprateek721@gmail.com>
2026-06-28 08:27:25 +0530

    Create Prefix_To_Infix.java

---
+++ StackAndQueue/Prefix_To_Infix.java
@@ -0,0 +1,25 @@
+class Solution {
+    static String preToInfix(String s) {
+      int i = s.length()-1;
+      Stack<String> st = new Stack<>();
+      while(i>=0)
+        {
+          char ch = s.charAt(i);
+          if((ch >='A' && ch<='Z') || (ch >='a' && ch<='z') || (ch >='0' && ch<='9'))
+        {
+          st.push(String.valueOf(ch));
+        }
+          else 
+          {
+            String t1 = st.peek();
+            st.pop();
+            String t2 = st.peek();
+            st.pop();
+            String ans = '(' + t1 + ch + t2 + ')';
+            st.push(ans);
+          }
+          i--;
+        }
+      return st.peek();
+    }
+}
