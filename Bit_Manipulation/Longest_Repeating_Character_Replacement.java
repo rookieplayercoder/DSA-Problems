d81ae5e1d3493eff50905498463e021442c15be7
Prateek Yadav <yprateek721@gmail.com>
2026-08-23 21:49:03 +0530

    Fix syntax errors in Longest_Repeating_Character_Replacement

--- Bit_Manipulation/Longest_Repeating_Character_Replacement.java
+++ Bit_Manipulation/Longest_Repeating_Character_Replacement.java
@@ -8,10 +8,10 @@ class Solution{
     for(int right = 0;right<s.length();right++)
       {
         map.put(s.charAt(right),map.getOrDefault(s.charAt(right))+1);
-        maxfreq = Math.max(maxfreq,map.get(s.charAt(right));
+        maxfreq = Math.max(maxfreq,map.get(s.charAt(right)));
         while((right-left+1)-maxfreq > k)
           {
-            map.put(s.charAt(left),map.get(s.charAt(left)-1);
+            map.put(s.charAt(left),map.get(s.charAt(left))-1);
             left++;
           }
         maxCount = Math.max(maxCount,right-left+1);
