c68aa9d267480b1509f998fd64224df3947f161d
Prateek Yadav <yprateek721@gmail.com>
2026-08-23 20:45:10 +0530

    Correct syntax and logic in totalFruits method
    Fix syntax errors and improve basket update logic.

--- Bit_Manipulation/Fruit_Into_Basket.java
+++ Bit_Manipulation/Fruit_Into_Basket.java
@@ -3,11 +3,11 @@ class Solution
     public int totalFruits(int[] fruits)
     {
       int maxCount = 0;
-      int left = 0
+      int left = 0;
       Map<Integer,Integer> basket = new HashMap<>();
       for(int right = 0;right<fruits.length;right++)
         {
-          basket.put(fruits[right],basket.getOrDefault(fruits[right])+1);
+          basket.put(fruits[right],basket.getOrDefault(fruits[right],0)+1);
           while(basket.size()>2)
             {
               basket.put(fruits[left],basket.get(fruits[left])-1);
