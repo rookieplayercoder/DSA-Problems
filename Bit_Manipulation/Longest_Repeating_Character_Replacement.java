6b3d8882bb8db5f0d65eab4bf48e1f22d04984d6
Prateek Yadav <yprateek721@gmail.com>
2026-08-23 21:44:11 +0530

    Add characterReplacement method for substring analysis
    Implement character replacement algorithm to find the longest substring with repeating characters.

---
+++ Bit_Manipulation/Longest_Repeating_Character_Replacement.java
@@ -0,0 +1,21 @@
+class Solution{
+  public int characterReplacement(String s)
+  {
+    int left = 0;
+    int maxCount = 0;
+    int maxfreq = 0;
+    Map<Character,Integer> map = new HashMap<>();
+    for(int right = 0;right<s.length();right++)
+      {
+        map.put(s.charAt(right),map.getOrDefault(s.charAt(right))+1);
+        maxfreq = Math.max(maxfreq,map.get(s.charAt(right));
+        while((right-left+1)-maxfreq > k)
+          {
+            map.put(s.charAt(left),map.get(s.charAt(left)-1);
+            left++;
+          }
+        maxCount = Math.max(maxCount,right-left+1);
+      }
+    return maxCount;
+  }
+}
