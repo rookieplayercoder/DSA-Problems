b762c18cb928e00b6d512e362a6cc04853172612
Prateek Yadav <yprateek721@gmail.com>
2026-08-27 12:59:41 +0530

    Create Count_No_Of_Nice_SubArrays.java

---
+++ Bit_Manipulation/Count_No_Of_Nice_SubArrays.java
@@ -0,0 +1,14 @@
+class Solution{
+   public int numberOfSubarrays(int[] nums, int k) {
+     Map<Integer,Integer> map = new HashMap<>();
+     int count = 0;
+     int sum = 0;
+     for(int right = 0;right< nums.length;right++)
+       {
+         int i = nums[right]++;
+         sum+=map.getOrDefault(sum-k,0);
+         map.put(sum,map.getOrDefault(sum,0)+1);
+       }
+     return count;
+   }
+}
